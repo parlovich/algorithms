34179bea8d838961936e5fa4944b593bfc6f75a2
Pavel_Arlovich <Pavel_Arlovich@epam.com>
2016-09-15 18:22:50 +0300

    RandomizedQueue initial implementation

--- Randomized Queues and Deques/src/RandomizedQueue.java
+++ Randomized Queues and Deques/src/RandomizedQueue.java
@@ -1,9 +1,38 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    private int size = 0;
+    private int begin = 0;
+    private Item[] items = (Item[]) new Object[1];
+
+    private class RandomizedQueueIterator implements Iterator<Item> {
+        int[] indexes = new int[size];
+
+        public RandomizedQueueIterator() {
+            for(int i = 0; i < size; i++) {
+                indexes[i] = i;
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public Item next() {
+            return null;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Removing is not supported");
+        }
+    }
+
     // construct an empty randomized queue
     public RandomizedQueue() {
-
     }
 
     // is the queue empty?
@@ -18,11 +47,19 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     // add the item
     public void enqueue(Item item) {
-
+        if (item == null) {
+            throw new NullPointerException("item must not be null");
+        }
+        if (size == items.length)
+            resize(2 * items.length);
+        items[size++] = item;
     }
 
     // remove and return a random item
     public Item dequeue() {
+        if (size == 0) {
+            throw new NoSuchElementException("Queue is empty");
+        }
         return null;
     }
 
@@ -33,7 +70,14 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     // return an independent iterator over items in random order
     public Iterator<Item> iterator() {
-        return null;
+        return new RandomizedQueueIterator();
+    }
+
+    private void resize(int capacity) {
+        Item[] copy = (Item[]) new Object[capacity];
+        for (int i = 0; i < size(); i++)
+            copy[i] = items[i];
+        items = copy;
     }
 
     // unit testing
