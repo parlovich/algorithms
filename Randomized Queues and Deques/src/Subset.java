4d538129b8911525d968c39fd3c143ac4035b477
Pavel_Arlovich <Pavel_Arlovich@epam.com>
2016-09-16 13:18:04 +0300

    Subset

--- Randomized Queues and Deques/src/Subset.java
+++ Randomized Queues and Deques/src/Subset.java
@@ -1,6 +1,22 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
 
 public class Subset {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            StdOut.println("arguments: <k>");
+            return;
+        }
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> q = new RandomizedQueue<>();
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            if ("-".equals(s)) break;
+            q.enqueue(s);
+        }
+        for(int i = 0; i < k; i++) {
+            StdOut.println(q.dequeue());
+        }
     }
 }
