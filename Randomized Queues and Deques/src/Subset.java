db8d445c5c33aa97047c6f1b1bc5ae0b79b4ba75
Pavel_Arlovich <Pavel_Arlovich@epam.com>
2016-09-17 13:02:46 +0300

    bonus

--- Randomized Queues and Deques/src/Subset.java
+++ Randomized Queues and Deques/src/Subset.java
@@ -1,5 +1,6 @@
-import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
 public class Subset {
 
@@ -10,10 +11,26 @@ public class Subset {
         }
         int k = Integer.parseInt(args[0]);
         RandomizedQueue<String> q = new RandomizedQueue<>();
+
+        // j - number of strings read from input
+        int j = 1;
         while (!StdIn.isEmpty()) {
             String s = StdIn.readString();
             if ("-".equals(s)) break;
-            q.enqueue(s);
+
+            if (j <= k) q.enqueue(s);
+            else {
+                // The likelihood of the strings to be in the queue should be equal to K / N
+                // likelihood of the string not be dequeued from the queue on j iteration is 1 - (1/k)(k/j) = (j-1)/j
+                // likelihood of the new string to be queued is k/j
+                // so after N iterations the strings will stay in the queue with likelihood:
+                //          ((n - 1)! / (k - 1)!) / (n! / k!) = k / n
+                if (StdRandom.uniform(j) < k) {
+                    q.dequeue();
+                    q.enqueue(s);
+                }
+            }
+            j++;
         }
         for (int i = 0; i < k; i++) {
             StdOut.println(q.dequeue());
