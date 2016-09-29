60ff6e22e0d949ad9418b8b0d22d519f81b474c2
Pavel_Arlovich <Pavel_Arlovich@epam.com>
2016-09-29 20:02:36 +0300

    Board initial implementation

--- Puzzle/src/Board.java
+++ Puzzle/src/Board.java
@@ -1,32 +1,49 @@
+import edu.princeton.cs.algs4.*;
+
 public class Board {
+    private int[][] blocks;
+    int n; // dimension
+
     // construct a board from an n-by-n array of blocks
     // (where blocks[i][j] = block in row i, column j)
     public Board(int[][] blocks) {
-        //TODO
+        this.blocks = blocks;
+        n = blocks.length;
     }
 
     // board dimension n
     public int dimension() {
-        //TODO
-        return 0;
+        return n;
     }
 
     // number of blocks out of place
     public int hamming() {
-        //TODO
-        return 0;
+        int r = 0;
+        for (int i = 0; i < n; i++) {
+            int k = i == n - 1 ? n - 1 : n;
+            for (int j = 0; j < k; j++) {
+                if (blocks[i][j] != i * n + j + 1) r++;
+            }
+        }
+        return r;
     }
 
     // sum of Manhattan distances between blocks and goal
     public int manhattan() {
-        //TODO
-        return 0;
+        int r = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (blocks[i][j] != 0 && blocks[i][j] != i * n + j + 1) {
+                    r += Math.abs(i - blocks[i][j] / n) + Math.abs(j + 1 - blocks[i][j] % n);
+                }
+            }
+        }
+        return r;
     }
 
     // is this board the goal board?
     public boolean isGoal() {
-        //TODO
-        return false;
+        return hamming() == 0;
     }
 
     // a board that is obtained by exchanging any pair of blocks
@@ -38,8 +55,15 @@ public class Board {
     // does this board equal y?
     @Override
     public boolean equals(Object y) {
-        //TODO
-        return false;
+        if (!(y instanceof Board)) return false;
+        if (n != ((Board) y).dimension()) return false;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (blocks[i][j] != ((Board) y).blocks[i][j])
+                    return false;
+            }
+        }
+        return true;
     }
 
     // all neighboring boards
@@ -51,12 +75,27 @@ public class Board {
     // string representation of this board (in the output format specified below)
     @Override
     public String toString() {
-        //TODO
-        return null;
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            if (i != 0) s.append("\n");
+            for (int j = 0; j < n; j++) {
+                if (j != 0) s.append(" ");
+                s.append(blocks[i][j] == 0 ? " " : blocks[i][j]);
+            }
+        }
+        return s.toString();
     }
 
     // unit tests (not graded)
     public static void main(String[] args) {
-
+        int[][] initial = new int [][] {
+                {8, 1, 3},
+                {4, 0, 2},
+                {7, 6, 5},
+        };
+        Board b = new Board(initial);
+        assert b.hamming() == 5;
+        assert b.manhattan() == 10;
+        StdOut.println(b);
     }
 }
